a34be222a37e8bc73a73409e7db77055f1236fca
Marion <mmadanguit@olin.edu>
2023-04-24 12:49:20 -0400

    Create simulation architecture

--- model.py
+++ model.py
@@ -1,32 +1,43 @@
 import random
+import numpy as np
 
 class Firefly:
-    """Represents a single firefly."""
-    def __init__(self, position, internal_clock):
-        self.position = position
-        self.internal_clock = internal_clock
+    """Represents a single firefly"""
+    def __init__(self, position):
+        self.position = position # Current position
+        self.remaining_time = 0 # Remaining time in the box
         
-    def flash(self):
-        """Prints a message indiciating that the firefly has flashed at the current time."""
-        print(f"Firefly {self.position} flashing at time {self.internal_clock}")
+    def move(self, n_sides):
+        """Updates the firefly's position according to the polygon dynamics"""
+        # TODO: Move each firefly forward according to the number of sides of the polygon, e.g. when a firefly is is on a box on the first side, it advances one position.
         
-    def nudge_clock(self, amount):
-        """Adds a small amount to the firefly's internal clock."""
-        self.internal_clock += amount
+    def flash(self): 
+        """Indicates that the firefly has flashed"""
+        print(f"Firefly at position {self.position} flashing")
+        
+    def decrement_time(self):
+        """Reduces the remaining time in the flashing box by 1"""
+        self.remaining_time = max(0, self.remaining_time - 1)
+        
+    def set_time(self, time):
+        """Sets the remaining time in the flashing box to a given value"""
+        self.remaining_time = time
 
-def simulate_fireflies(num_fireflies, max_time, nudge_amount):
-    """Stimulate a group of fireflies."""
-    fireflies = [Firefly(i, random.randint(1, max_time)) for i in range(num_fireflies)] # Create a list of Firefly objects with random internal clocks
-    for t in range(1, max_time+1): # Loop through each time step from 1 to max_time
-        for i in range(num_fireflies): 
-            if fireflies[i].internal_clock % 12 == 0: # At each time step, check if any firefly's internal clock has reached the designated flashing time, in this case 12
-                fireflies[i].flash() 
-                for j in range(num_fireflies): # Check for nearby fireflies that should be nudged
-                    if i != j and abs(fireflies[i].position - fireflies[j].position) == 1:
-                        fireflies[j].nudge_clock(nudge_amount) 
+def simulate_fireflies(num_fireflies, n_sides, flashing_box_index, max_time):
+    """Simulates a group of fireflies and their movements for a given number of time steps. 
+    Returns a 2D array indicated when each firefly flashed at each timestep"""
+    fireflies = [Firefly(i) for i in range(num_fireflies) if i != flashing_box_index] # Create a list of Firefly objects for all firefles except the flashing box
+    flashes = np.zeros((num_fireflies, max_time)) # Create an array to record the fireflies that flash at each timestep
+    for t in range(max_time): 
+        pass
+        # If no fireflies are in the flashing box, move each firefly one position forward
+        # If there is a firefly in the flashing box, it stays in the flashing box for one time step and each firefly moves forward according to the number of sides of the polygon.
+        
+    return flashes
 
 """
 TODO:
+-Finish implementing simulate_fireflies
 -Visualize fireflies flashing
 -Make fireflies position dynamic
 -Enable to user to:
@@ -38,4 +49,5 @@ TODO:
 
 if __name__ == '__main__':
 
-    simulate_fireflies(num_fireflies=5, max_time=100, nudge_amount=1)
+    flashes = simulate_fireflies(num_fireflies=5, n_sides=5, flashing_box_index=4, max_time=100)
+    print(flashes)
