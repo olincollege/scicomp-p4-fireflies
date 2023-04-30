e2f38b63e05aba79d3aa557372f64e2f1e68bff4
Marion <mmadanguit@olin.edu>
2023-04-30 13:48:05 -0400

    Add visualization infrastructure

--- model.py
+++ model.py
@@ -1,45 +1,106 @@
 import random
 import numpy as np
 
-class Firefly:
-    """Represents a single firefly"""
-    def __init__(self, position):
-        self.position = position # Current position
-        self.remaining_time = 0 # Remaining time in the box
-        
-    def move(self, n_sides):
-        """Updates the firefly's position according to the polygon dynamics"""
-        # TODO: Move each firefly forward according to the number of sides of the polygon, e.g. when a firefly is is on a box on the first side, it advances one position.
-        
-    def flash(self): 
-        """Indicates that the firefly has flashed"""
-        print(f"Firefly at position {self.position} flashing")
-        
-    def decrement_time(self):
-        """Reduces the remaining time in the flashing box by 1"""
-        self.remaining_time = max(0, self.remaining_time - 1)
-        
-    def set_time(self, time):
-        """Sets the remaining time in the flashing box to a given value"""
-        self.remaining_time = time
+from time import time
+import matplotlib.pyplot as plt
 
-def simulate_fireflies(num_fireflies, n_sides, flashing_box_index, max_time):
-    """Simulates a group of fireflies and their movements for a given number of time steps. 
-    Returns a 2D array indicated when each firefly flashed at each timestep"""
-    fireflies = [Firefly(i) for i in range(num_fireflies) if i != flashing_box_index] # Create a list of Firefly objects for all firefles except the flashing box
-    flashes = np.zeros((num_fireflies, max_time)) # Create an array to record the fireflies that flash at each timestep
-    for t in range(max_time): 
-        pass
-        # If no fireflies are in the flashing box, move each firefly one position forward
-        # If there is a firefly in the flashing box, it stays in the flashing box for one time step and each firefly moves forward according to the number of sides of the polygon.
+class Simulation:
+    """Simulates a group of fireflies flashing"""
+    def __init__(self, num_fireflies, num_sides, max_time):
+        ### MODELING VARIABLES ###
+        self.num_fireflies = num_fireflies
+        self.num_sides = num_sides
+        self.max_time = max_time
+
+        ### PLOTTING VARIABLES ### 
+        self.frame_count = 0
+        self.fireflies = None # Array with 4 columns: x, y, time, and flashing (0 or 1)
+
+        plt.ion()
+
+        self.fig = plt.figure(figsize=(5, 5))
+        self.ax = self.fig.add_subplot(111)
+
+    def add_fireflies(self):
+        """Add fireflies to self.fireflies"""
+        # TODO: Initialize with random position and random internal clock
+        fireflies = [Firefly(i) for i in range(num_fireflies) if i != flashing_box_index] # Create a list of randomly initialized fireflies
+
+
+    def move_fireflies(self):
+        """Move fireflies in a random direction"""
+        # TODO: Move fireflies in a random direction
+
+    def simulate_timestep(self):
+        """Flashing and position"""
+        # TODO: Make flashing and position calculations, call self.move_fireflies()
         
-    return flashes
+
+    def clear_graph(self):
+        """Clear graph and set limits"""
+        self.ax.clear()
+        self.ax.set_xlim([0, self.size])
+        self.ax.set_ylim([0, self.size])
+
+    def run(self):
+        """Run and plot simulation"""
+        # Add fireflies
+        self.add_fireflies()
+
+        # Start FPS counter for initial run
+        frame_time_start = time()
+
+        while True: 
+            ### FLASHING AND POSITION CALCULATIONS ####
+            self.simulate_timestep()
+
+            ### PLOTTING ###
+            self.clear_graph()
+
+            # Get firefly positions
+            x = self.fireflies[:, 0]
+            y = self.fireflies[:, 1]
+
+            # Set flashing fireflies to yellow
+            colors = np.full(len(self.fireflies), 'blue')
+            flashing_firefly_indices = np.where(self.fireflies[:, 3])
+            colors[flashing_firefly_indices] = 'red' # TODO: Make array of colors
+        
+            # Plot fireflies
+            self.ax.scatter(x, y, s=5, color=colors)
+            
+            # Compute and show FPS 
+            frame_time_end = time()
+            fps = 1 / (frame_time_end - frame_time_start)
+            text_fps_string = f'FPS: {fps:.1f}'
+            text_fps = self.fig.text(.12, .025, text_fps_string, fontsize=10)
+
+            # Show number of fireflies
+            text_num_fireflies_string = f'Fireflies: {self.num_fireflies}'
+            text_num_fireflies = self.fig.text(.4, .025, text_num_fireflies_string, fontsize=10)
+
+            # Show number of flashing fireflies
+            num_flashing = np.count_nonzero(self.fireflies[:, 3])
+            text_num_flashing_string = f'Flashing: {num_flashing}'
+            text_num_flashing = self.fig.text(.68, .025, text_num_flashing_string, fontsize=10)
+
+            # DRaw on canvas
+            self.fig.canvas.draw()
+            self.fig.canvas.flush_events()
+
+            # Reset fps timer
+            frame_time_start = time()
+
+            # Remove old text
+            text_fps.remove()
+            text_num_fireflies.remove()
+            text_num_flashing.remove()
+
+            self.frame_count += 1
+
 
 """
 TODO:
--Finish implementing simulate_fireflies
--Visualize fireflies flashing
--Make fireflies position dynamic
 -Enable to user to:
     -Adjust the number of fireflies
     -Toggle between whether nudging neighbors adjusts their internal clock
